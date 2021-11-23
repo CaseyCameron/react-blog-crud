dc0b18eafb7d244e5eb704d9bea72e19edce80cc
Casey Cameron <mcasey.cameron@gmail.com>
2021-11-23 10:21:52 -0800

    Router setup

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
-import Home from './views/Home/Home'; 
+import Home from './views/Home/Home';
+import Detail from './views/Detail/Detail.js';
 
 import { getBlogs } from './services/blogs';
+import { Switch, Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   const [blogs, setBlogs] = useState([]);
-  
-  useEffect(()=>{
-    const fetchData = async()=>{
+
+  useEffect(() => {
+    const fetchData = async () => {
       const resp = await getBlogs();
       setBlogs(resp);
     };
@@ -21,9 +23,16 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <div className="main">
-        <Home blogs={blogs} />
-      </div>
+      <Switch>
+        <div className="main">
+          <Route path='/'>
+            <Home blogs={blogs} />
+          </Route>
+          <Route path='/blogs/:id'>
+            <DetailPage />
+          </Route>
+        </div>
+      </Switch>
       <Footer />
     </BrowserRouter>
   );
